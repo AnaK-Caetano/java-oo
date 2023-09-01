55dc4109bc8a71357eaad6acafdc142d788fe6a3
Ana Karina Caetano <103366118+AnaK-Caetano@users.noreply.github.com>
2023-09-01 23:04:35 +0000

    exercícios - java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Olá mundo!");
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // 01
+        // int numero = sc.nextInt();
+
+        // if (numero%2 == 0){
+        //     System.out.println("É um número par");
+        // } else {
+        //     System.out.println("Não é par!");
+        // }
+
+        // 02
+
+        System.out.println("Digite o lado 1");
+        int lado1 = sc.nextInt();
+
+        System.out.println("Digite o lado 2");
+        int lado2 = sc.nextInt();
+
+        System.out.println("Digite o lado 3");
+        int lado3 = sc.nextInt();
+
+        if (
+        } else if {
+
+        }
+
     }
-}
+    }
